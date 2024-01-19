d0e43bb2d55cb65e42061ae95ef597b431f97800
Jennifer Gardner <jeng8942@yahoo.com>
2024-01-19 13:30:31 +0000

    JavaDoc updates for Controller

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -27,10 +27,8 @@ import com.example.service.AccountService;
 import com.example.service.MessageService;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ * Endpoints and handlers for your controller using Spring. The endpoints use the @GET/POST/PUT/DELETE/etc 
+ * Mapping annotations where applicable as well as the @ResponseBody and @PathVariable annotations. 
  */
 @Controller
 public class SocialMediaController {
@@ -46,8 +44,7 @@ public class SocialMediaController {
     /**
      * Process new User registrations.
      * As a user, I should be able to create a new Account on the endpoint POST localhost:8080/register. 
-     * The registration will be successful if and only if the username is not blank, the password is at least 
-     * 4 characters long, and an Account with that username does not already exist. 
+     * The registration will be successful if it meets the requirments of AccountService.Register().
      * 
      * @param account The Account object created from the request body which will contain a representation of a JSON 
      *                Account, but will not contain an account_id.
@@ -77,18 +74,18 @@ public class SocialMediaController {
 
 
     /**
-     * ## 2: Our API should be able to process User logins.
-     * 
+     * Process User logins.
      * As a user, I should be able to verify my login on the endpoint POST localhost:8080/login. 
-     * The request body will contain a JSON representation of an Account.
-
-- The login will be successful if and only if the username and password provided in the request body JSON 
-match a real account existing on the database. If successful, the response body should contain a JSON of the account in the response body, including its account_id. The response status should be 200 OK, which is the default.
-- If the login is not successful, the response status should be 401. (Unauthorized)
-*/
+     * The login will be sucessful if the username and password meet the requirements in AccountService.Login()
+     *
+     * @param account created vrom the request body which will contain a JSON representation of an Account.
+     * @return ResponseEntity 
+     * - If successful, the response body should contain a JSON of the account in the response body, including its account_id. 
+     *   The response status should be 200 OK, which is the default. 
+     * - If the login is not successful, the response status should be 401. (Unauthorized)
+     */
     @PostMapping(value="/login")
     public @ResponseBody ResponseEntity<Account> login(@RequestBody Account account) {
-        //TODO
         try {
             Account newAccount = accountService.login(account);
             return ResponseEntity.status(200).body(newAccount);
@@ -99,19 +96,19 @@ match a real account existing on the database. If successful, the response body 
     }
 
     /**
-     * ## 3: Our API should be able to process the creation of new messages.
-     * 
-     * As a user, I should be able to submit a new post on the endpoint POST localhost:8080/messages. 
-     * The request body will contain a JSON representation of a message, which should be persisted to the database, 
-     * but will not contain a message_id.
-
-- The creation of the message will be successful if and only if the message_text is not blank, is not over 255 characters, 
-and posted_by refers to a real, existing user. If successful, the response body should contain a JSON of the message, including its message_id. The response status should be 200, which is the default. The new message should be persisted to the database.
-- If the creation of the message is not successful, the response status should be 400. (Client error)
-*/
+     * Process the creation of new messages.
+     * As a user, I should be able to submit a new post on the endpoint POST localhost:8080/messages.
+     * The creation of the message will be successful if the message meets the requirements in MessageService.postMessage
+     *
+     * @param message created from the request body which will contain a JSON representation of a message,
+     *  but will not contain a message_id.
+     * @return ResponseEntity 
+     * - If successful, the response body should contain a JSON of the message, including its message_id. 
+     *   The response status should be 200, which is the default. The new message should be persisted to the database.
+     * - If the creation of the message is not successful, the response status should be 400. (Client error)
+     */
     @PostMapping(value="/messages")
     public @ResponseBody ResponseEntity<Message> postMessage(@RequestBody Message message) {
-        //TODO
         if(message != null){ 
             try {
                 Message newMessage = messageService.postMessage(message);
@@ -127,13 +124,14 @@ and posted_by refers to a real, existing user. If successful, the response body 
     }
 
     /**
-     * ## 4: Our API should be able to retrieve all messages.
-     * 
+     * Retrieve all messages.
      * As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages.
-
-- The response body should contain a JSON representation of a list containing all messages retrieved from the database. 
-It is expected for the list to simply be empty if there are no messages. The response status should always be 200, which is the default.
-*/
+     * 
+     * @return ResponseEntity
+     * - The response body should contain a JSON representation of a list containing all messages retrieved from the database. 
+     * - It is expected for the list to simply be empty if there are no messages. T
+     * The response status should always be 200, which is the default.
+     */
     @GetMapping(value="/messages")
     public @ResponseBody ResponseEntity<List<Message>> getMesssages() {
         //TODO
@@ -142,71 +140,75 @@ It is expected for the list to simply be empty if there are no messages. The res
     }
 
     /**
-     * ## 5: Our API should be able to retrieve a message by its ID.
-
-As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages/{message_id}.
-
-- The response body should contain a JSON representation of the message identified by the message_id. It is expected for 
-the response body to simply be empty if there is no such message. The response status should always be 200, which is the default.
-
-*/
+     * Retrieve a message by its ID.
+     * As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages/{message_id}.
+     * 
+     * @param message_id as a path variable
+     * @return ResponseEntity
+     * The response body should contain a JSON representation of the message identified by the message_id. 
+     * It is expected for the response body to simply be empty if there is no such message. 
+     * The response status should always be 200, which is the default.
+     */
     @GetMapping(value="/messages/{message_id}")
     public @ResponseBody ResponseEntity<Message> getMessage(@PathVariable int message_id) {
         //TODO
         Message newMessage = messageService.getMessage(message_id);
+        
         return ResponseEntity.status(200).body(newMessage);
     }
     
 
     /**
-     * ## 6: Our API should be able to delete a message identified by a message ID.
+     * Delete a message identified by a message ID.
      * 
      * As a User, I should be able to submit a DELETE request on the endpoint DELETE localhost:8080/messages/{message_id}.
-
-- The deletion of an existing message should remove an existing message from the database. If the message existed,
- the response body should contain the number of rows updated (1). The response status should be 200, which is the default.
-- If the message did not exist, the response status should be 200, but the response body should be empty. 
-This is because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should respond with the same type of response.
-
-*/
+     * The deletion of an existing message should remove an existing message from the database.
+     * 
+     * @param message_id as a path variable
+     * @return ResponseEntity
+     * - If the message existed, the response body should contain the number of rows updated (1). 
+     * - If the message did not exist, the response body should be empty. 
+     * The response status should be 200, which is the defaultThis is because the DELETE verb is intended to be idempotent, 
+     * ie, multiple calls to the DELETE endpoint should respond with the same type of response.
+     */
     @DeleteMapping(value="/messages/{message_id}")
     public @ResponseBody ResponseEntity<Integer> deleteMessage(@PathVariable Integer message_id) {
-        // TODO
         int retVal = messageService.deleteMessage(message_id);
-        if(retVal == 1) {
+        if(retVal > 0) {
             return ResponseEntity.status(200).body(1);
         }
         else {
             return ResponseEntity.status(200).build();
         }
-        
     }
 
     /**
-     * ## 7: Our API should be able to update a message text identified by a message ID.
+     * Update a message text identified by a message ID.
      * As a user, I should be able to submit a PATCH request on the endpoint PATCH localhost:8080/messages/{message_id}. 
-     * The request body should contain a new message_text values to replace the message identified by message_id. 
-     * The request body can not be guaranteed to contain any other information.
+     * The update of a message should be successful if and only if the parameters meet the requirements 
+     * in MessageService.updateMessage. The message existing in the database should have the updated message_text.
      * 
-     * The update of a message should be successful if and only if the message id already exists and the new message_text 
-     * is not blank and is not over 255 characters. If the update is successful, the response body should contain the number 
-     * of rows updated (1), and the response status should be 200, which is the default. The message existing on the database 
-     * should have the updated message_text.
-     * If the update of the message is not successful for any reason, the response status should be 400. (Client error)
-     * 
-     * @param message_id A unique identifier for the message (primary key)
-     * @return Message
+     * @param message_id as a path variable. A unique identifier for the message (primary key)
+     * @param message_text The request body should contain json name value pairs with the value for
+     *        message_text. The request body can not be guaranteed to contain any other information. 
+     * @return ResponseEnity
+     * - If the update is successful, the response body should contain the number of rows updated (1), and the response 
+     *   status should be 200, which is the default. 
+     * - If the update of the message is not successful for any reason, the response status should be 400. (Client error)
      */
     @PatchMapping(value="/messages/{message_id}") 
     public @ResponseBody ResponseEntity<Integer> updateMessage(@PathVariable Integer message_id, @RequestBody Map message_text) {
-        //TODO
         String text = null;
         if(message_text != null) {
             text = (String) message_text.get("message_text");
         }
         try {
             Integer rows = messageService.updateMessage(message_id, text);
-            return ResponseEntity.status(200).body(rows);
+            if(rows > 0) {
+                return ResponseEntity.status(200).body(rows);
+            } else {
+                return ResponseEntity.status(400).build();
+            }   
         }
         catch(RuntimeException e) {
             return ResponseEntity.status(400).build();
@@ -214,19 +216,19 @@ This is because the DELETE verb is intended to be idempotent, ie, multiple calls
     }
 
     /**
-     * ## 8: Our API should be able to retrieve all messages written by a particular user.
-
-As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/accounts/{account_id}/messages.
-
-- The response body should contain a JSON representation of a list containing all messages posted by a particular user, 
-which is retrieved from the database. It is expected for the list to simply be empty if there are no messages. The response status should always be 200, which is the default.
-
-   */
+     * Retrieve all messages written by a particular user.
+     * As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/accounts/{account_id}/messages.
+     * 
+     * @param account_id as a path variable
+     * @return ResponseEntity
+     * - The response body should contain a JSON representation of a list containing all messages posted by a particular user, 
+     *   which is retrieved from the database. 
+     * - It is expected for the list to simply be empty if there are no messages. 
+     * The response status should always be 200, which is the default. 
+     */
     @GetMapping(value="/accounts/{account_id}/messages")
-    public @ResponseBody ResponseEntity<List<Message>> getAccountMessages(@PathVariable int account_id) {
-        //TODO
+    public @ResponseBody ResponseEntity<List<Message>> getAccountMessages(@PathVariable Integer account_id) {
         List<Message> messages = messageService.getAccountMessages(account_id);
         return ResponseEntity.status(200).body(messages);
     }
-
 }
